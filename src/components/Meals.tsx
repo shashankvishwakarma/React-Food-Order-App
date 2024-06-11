43e9434c78ecfc9407eb66d2820c2e4540f8d825
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2024-06-11 18:14:37 +0530

    added menu and menu item

---
+++ src/components/Meals.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+import MealItem from "./MealItem";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    // @ts-ignore
+    async function fetchMeals() {
+      const response = await fetch('http://localhost:3000/meals');
+
+      if (!response.ok) {
+        // ...
+      }
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+      <ul id="meals">
+        {loadedMeals.map((meal) => (
+            <MealItem key={meal.id} meal={meal}/>
+        ))}
+      </ul>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+// @ts-ignore
 import React from 'react'
+// @ts-ignore
 import ReactDOM from 'react-dom/client'
-
-import App from './App.jsx'
+import App from "./App"
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <App/>
     </React.StrictMode>,
